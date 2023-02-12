ebc14389dcf83db33dc1bc7ea24037b284f4ba18
antorahad <113632611+antorahad@users.noreply.github.com>
2023-02-12 19:55:24 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+//filter the post
+$(document).ready(function() {
+    $('.filter-item').click(function() {
+        const value = $(this).attr('data-filter')
+        if (value == "all") {
+            $('.post-box').show("1000");
+        }else {
+            $('.post-box')
+            .not('.' + value)
+            .hide("1000");
+            $('.post-box')
+            .filter('.' + value)
+            .show("1000");
+        }
+    });
+
+    //active button
+    $('.filter-item').click(function(){
+        $(this).addClass('active-filter').siblings().removeClass('active-filter');
+    })
+});
+
+//navbar color change
+let header = document.querySelector('header');
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
